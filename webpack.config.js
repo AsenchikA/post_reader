8618b92479b84e88e34a0dedd05b4636c569701e
Anastasiya <n-asenchik@yandex.ru>
2022-05-14 19:51:23 +0300

    fix ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx$/,
+        test: /\.tsx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'ts-loader',
